21e9019ee1a3df51ef90690236f20d81fdde264b
GemShi <gemshi@GemShideMacBook-Pro.local>
2017-03-20 17:52:50 +0800

    Initial Commit

---
+++ GemCustomMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GemCustomMap
+//
+//  Created by GemShi on 2017/3/20.
+//  Copyright © 2017年 GemShi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
